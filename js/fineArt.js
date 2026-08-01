28c43227ec0566b528331e228c1a4cc38d717de3
alexfarm08 <209876587+alexfarm08@users.noreply.github.com>
2026-08-01 15:14:43 -0700

    added fineArt.js and finished fine art page just need to do some touch ups

---
+++ js/fineArt.js
@@ -0,0 +1,22 @@
+const productDisplay = document.querySelector('.product-display');
+
+const products = Array.from({ length: 30 }, (_, i) => ({
+    image: `/Images/photos/product.jpg`,
+    name: 'Piece Name',
+    location: 'Location Name, State/Province',
+    price: '$995'
+}));
+
+products.forEach(product => {
+    const card = document.createElement('div');
+    card.className = 'product-card';
+
+    card.innerHTML = `
+        <img src="${product.image}" alt="${product.name}">
+        <p class="product-name">${product.name}</p>
+        <p class="product-location">${product.location}</p>
+        <p class="product-price">${product.price}</p>
+    `;
+
+    productDisplay.appendChild(card);
+});
